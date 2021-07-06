dc23f3bdc20ed5db316179081d1f0a01a6ebbbb8
nniinima <niko.niinimaki@gmail.com>
2021-07-06 10:53:05 +0300

    fixed questions typos

--- questions.py
+++ questions.py
@@ -1,7 +1,7 @@
 quiz = {
     1: {
         "question": "What type of N1 VM can you use for free for 720 hours in GCP?",
-        "answer": "f1 micro"
+        "answer": "f1-micro"
     },
     2: {
         "question": "Which is the HTTPS traffic port?",
